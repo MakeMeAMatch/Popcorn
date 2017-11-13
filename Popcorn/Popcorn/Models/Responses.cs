ff349d426380d4dccd8a37a7b071eae13616fa87
Brandon Little <brlittle86@gmail.com>
2017-11-13 09:59:12 -0800

    created Responses and Matches models

---
+++ Popcorn/Popcorn/Models/Responses.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Popcorn.Models
+{
+    public class Responses
+    {
+        public int Id { get; set; }
+        public int Religion { get; set; }
+        public int Politics { get; set; }
+        public int Sports { get; set; }
+        public int Diet { get; set; }
+        public int Entertainment { get; set; }
+        public int HonestySpectrum { get; set; }
+    }
+}
