3330b4189bd38755b0420ed00b3da3dda638076a
Brandon Little <brlittle86@gmail.com>
2017-11-13 09:49:57 -0800

    created Users model

---
+++ Popcorn/Popcorn/Models/Users.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Popcorn.Models
+{
+    public class Users
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public int ResponsesId { get; set; }
+    }
+}
