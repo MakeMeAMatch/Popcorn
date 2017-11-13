e740bd4313bed9122ce52a6d2c3d09487035357c
Brandon Little <brlittle86@gmail.com>
2017-11-13 10:31:35 -0800

    created testing solution

---
+++ Popcorn/PopcornTests/UnitTest1.cs
@@ -0,0 +1,16 @@
+using System;
+using Xunit;
+
+namespace PopcornTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void CanCreateUser()
+        {
+            // Arrange
+            // Act
+            // Assert
+        }
+    }
+}
