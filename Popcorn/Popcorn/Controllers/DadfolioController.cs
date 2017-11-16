207b5ebf7e27adabf1cb2decc49b268f6c55a555
miyaluc <srlucas3@gmail.com>
2017-11-16 14:45:00 -0800

    added CRUD operations to Dadfolio Controller

--- Popcorn/Popcorn/Controllers/DadfolioController.cs
+++ Popcorn/Popcorn/Controllers/DadfolioController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Popcorn.Data;
 using Popcorn.Models;
 using System;
@@ -21,12 +22,16 @@ namespace Popcorn.Controllers
             _userManager = userManager;
         }
 
+        //GET
+        [HttpGet]
         public IActionResult Index()
         {
-            return View();
+            return View(_context.Profiles);
         }
 
-        public IActionResult TakeQuiz()
+        //POST
+        [HttpPost]
+        public async Task<IActionResult> TakeQuiz(Profiles profiles)
         {
             //var DbProfiles = _context.Profiles;
             //var currentUser = await _userManager.GetUserAsync(User);
@@ -36,8 +41,53 @@ namespace Popcorn.Controllers
             ViewBag.UserQuestions = _context.Questions;
             ViewBag.UserAnswers = _context.Answers;
 
-            return View();           
+            await _context.AddAsync(profiles);
+            await _context.SaveChangesAsync();
 
+            return RedirectToAction("Index");           
+
+        }
+
+        //PUT
+        [HttpPut]
+        public async Task<IActionResult> Put(int id, Profiles profiles)
+        {
+            if(!ModelState.IsValid)
+            {
+                BadRequest(ModelState);
+            }
+
+            var check = _context.Profiles.FirstOrDefault(w => w.Id == id);
+
+            if (check != null)
+            {
+                check.ApplicationUserId = profiles.ApplicationUserId;
+                check.AnswersId = profiles.AnswersId;
+                check.QuestionsId = profiles.QuestionsId;
+                check.ResponsesId = profiles.ResponsesId;
+                _context.Update(check);
+                await _context.SaveChangesAsync();
+                return CreatedAtAction("Index", new { id = profiles.Id }, profiles);
+            }
+            else if(check == null)
+            {
+                await TakeQuiz(profiles);
+            }
+            return BadRequest();
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var result = _context.Profiles.FirstOrDefault(w => w.Id == id);
+
+            if(result != null)
+            {
+                _context.Profiles.Remove(result);
+                await _context.SaveChangesAsync();
+                return Ok();
+            }
+            return BadRequest();
         }
     }
 }
