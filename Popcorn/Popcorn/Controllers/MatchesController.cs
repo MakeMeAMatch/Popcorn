688f957eef83e8a1e8dc9311726bdd1609052f5c
Brandon Little <brlittle86@gmail.com>
2017-11-16 14:27:36 -0800

    created matched user logic in MatchesController Index action

--- Popcorn/Popcorn/Controllers/MatchesController.cs
+++ Popcorn/Popcorn/Controllers/MatchesController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Popcorn.Data;
+using Popcorn.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,9 +11,25 @@ namespace Popcorn.Controllers
 {
     public class MatchesController : Controller
     {
-        public IActionResult Index()
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly ApplicationDbContext _context;
+        private readonly PopcornDbContext _popcornContext;
+
+        public async Task<IActionResult> Index()
         {
-            return View();
+            var currentUser = await _userManager.GetUserAsync(User);
+            var currentUserMatches = _popcornContext.Matches.Where(p => p.UserMatchedId == currentUser.Id);
+            List<ApplicationUser> matchedUsers = new List<ApplicationUser>();
+            foreach (var match in currentUserMatches)
+            {
+                var isMatch = _popcornContext.Matches.Where(j => j.UserMatchedId == match.UserMatchingId && j.UserMatchingId == currentUser.Id);
+                if (isMatch != null)
+                {
+                    var matchedUser = _context.ApplicationUser.Where(m => m.Id == isMatch.First().UserMatchedId);
+                    matchedUsers.Add(matchedUser.First());
+                }
+            }
+            return View(matchedUsers);
         }
     }
 }
